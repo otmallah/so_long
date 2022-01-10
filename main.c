94280b03bc38f67500bebe82b2d9d983f569e124
othmanmallah <88326652+otmallah@users.noreply.github.com>
2022-01-10 01:18:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,124 +12,170 @@
 
 #include "mlx.h"
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 
-int u;
-int d;
-int r;
-int l;
+int k;
+int k2;
 
-typedef struct	s_vars {
+typedef struct	s_long {
 	void	*mlx;
 	void	*win;
-	void	*img;
+	void	*img1;
 	void	*img2;
-	void	*img3;
-	void	*img4;
-	char	*path4;
-	int		height;
-	int		width;
-	int		x;
-	int		y;
 	char	*path;
-}				t_vars;
+	char	*path2;
+	int		x , y;
+	int fd;
+	char *s;
+	char *str;
+	int i , a;
+}	t_long;
 
-int	key_hook(int keycode, t_vars *vars)
+int    check_size(void)
 {
-	static int a = 1;
-	static int c = 1;
-	int b;
-	if (keycode == 126)
-	{
-		b = 1;
-		mlx_clear_window(vars->mlx, vars->win);
-		while (b < 1000)
-		{	
-			mlx_put_image_to_window(vars->mlx, vars->win, vars->img2, b, 625);
-			b += 50;
+	int k = 0, l = 0;
+	int fd;
+	char *str;
+
+	fd = open("test/test.ber" , O_RDONLY);
+	str = get_next_line(fd);
+	if (str == NULL)
+		return (0);
+	k = strlen(str);
+	while(str != NULL)  {
+		l = strlen(str);
+		if (l != k)
+		{
+			puts("map is not rectangular");
+			exit(1);
 		}
-		c -= 40;
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, a, c);
+		str = get_next_line(fd);
 	}
-	else if (keycode == 123)
-	{
-		mlx_clear_window(vars->mlx, vars->win);
-		while (b < 1000)
-		{	
-			mlx_put_image_to_window(vars->mlx, vars->win, vars->img2, b, 625);
-			b += 50;
-		}
-		a -= 40;
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img3, a, c);
-	}
-	else if (keycode == 124)
-	{
-		b = 1;
-		mlx_clear_window(vars->mlx, vars->win);
-		while (b < 1000)
-		{	
-			mlx_put_image_to_window(vars->mlx, vars->win, vars->img2, b, 625);
-			b += 50;
-		}
-		a += 40;
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, a, c);
-	}
-	else if (keycode == 125)
-	{
-		b = 1;
-		mlx_clear_window(vars->mlx, vars->win);
-		while (b < 1000)
-		{	
-			mlx_put_image_to_window(vars->mlx, vars->win, vars->img2, b, 625);
-			b += 50;
-		}
-		c += 40;
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img, a, c);
-	}
-	return (0);
+	return 0;
 }
 
-int	main(void)
+int	check_event(char *path, char *path2, t_long *index)
 {
-	t_vars	vars;
-	char *path = "test/output-onlinepngtools.xpm";
-	char *path3 = "test/output-onlinepngtools-_3_.xpm";
-	char *path2 = "test/output-onlinepngtools-_4_.xpm";
-	vars.path4 = "test/tttt.xpm";
-	int x;
-	int res;
-	int y;
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	int d = 1;
+	int p = 0;
+	int l = 0;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1000, 700, "wach");
-	vars.img = mlx_xpm_file_to_image(vars.mlx, path, &vars.width, &vars.height);
-	vars.img3 = mlx_xpm_file_to_image(vars.mlx, path3, &vars.x, &vars.y);
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 530, 1);
-	vars.img2 = mlx_xpm_file_to_image(vars.mlx, path2, &x, &y);
-	mlx_put_image_to_window(vars.mlx, vars.win, vars.img2, 1, 625);
-	while (a < 1000)
-	{	
-		mlx_put_image_to_window(vars.mlx, vars.win, vars.img2, a, 625);
-		a += 76;
+	index->i = 0;
+	index->a = 0;
+	while (index->str != NULL) {
+		while (index->str[index->i]) {
+			if (index->str[index->i] == '1')
+				mlx_put_image_to_window(index->mlx, index->win, index->img1, p, l);
+			else if (index->str[index->i] == '0')
+				printf("0");
+			else if (index->str[index->i] == 'E')
+				printf("e");
+			else if (index->str[index->i] == 'C')
+				printf("c");
+			else if (index->str[index->i] == 'P')
+			{
+				k = p;
+				k2 = l;
+				mlx_put_image_to_window(index->mlx, index->win, index->img2, p, l);
+			}
+			index->i++;
+			p += 75;
+		}
+		p = 0;
+		l += 76;
+		index->str = get_next_line(index->fd);
+		index->i = 0;
 	}
-	while (b < 1000)
-	{	
-		mlx_put_image_to_window(vars.mlx, vars.win, vars.img2, 1, b);
-		b += 76;
+	return 0;
+}
+
+int		check_event2(char *path, char *path2 , t_long *index)
+{
+	int e = 0;
+	int k = 0;
+
+	index->i = 0;
+	index->a = 0;
+	index->fd = open("test/test.ber" , O_RDONLY);
+	index->str = get_next_line(index->fd);
+	printf("%s" , index->str);
+	if (index->str == NULL)
+		return 0;
+	while (index->str != NULL) {
+		while (index->str[index->i])
+		{
+			puts("im here hh");
+			if (index->str[index->i] == '1')
+				mlx_put_image_to_window(index->mlx, index->win, index->img1, e, k);
+			else if (index->str[index->i] == '0')
+				printf("0");
+			else if (index->str[index->i] == 'E')
+				printf("e");
+			else if (index->str[index->i] == 'C')
+				printf("c");
+			index->i++;
+			e += 75;
+		}
+		e = 0;
+		k += 76;
+		index->str = get_next_line(index->fd);
+		index->i = 0;
 	}
-	while (c < 1000)
-	{	
-		mlx_put_image_to_window(vars.mlx, vars.win, vars.img2, c, 1);
-		c += 76;
+	return 0;
+}
+
+int		key_hook(int keycode, t_long *index)
+{
+	index->path = "test/output-onlinepngtools.xpm";
+	index->path2 = "test/open.xpm";
+
+	if (keycode == 65361)
+	{
+		mlx_clear_window(index->mlx, index->win);
+		check_event2(index->path, index->path2, index);
+		k -= 75;
+		mlx_put_image_to_window(index->mlx, index->win, index->img2, k, k2);
 	}
-	while (d < 1000)
-	{	
-		mlx_put_image_to_window(vars.mlx, vars.win, vars.img2, 930, d);
-		d += 76;
+	else if (keycode == 65363)
+	{
+		mlx_clear_window(index->mlx, index->win);
+		check_event2(index->path, index->path2, index);
+		k += 75;
+		mlx_put_image_to_window(index->mlx, index->win, index->img2, k, k2);
 	}
-	mlx_loop(vars.mlx);
+	else if (keycode == 65362)
+	{
+		mlx_clear_window(index->mlx, index->win);
+		check_event2(index->path, index->path2, index);
+		k2 -= 75;
+		mlx_put_image_to_window(index->mlx, index->win, index->img2, k, k2);
+	}
+	else if (keycode == 65364)
+	{
+		mlx_clear_window(index->mlx, index->win);
+		check_event2(index->path, index->path2, index);
+		k2 += 75;
+		mlx_put_image_to_window(index->mlx, index->win, index->img2, k, k2);
+	}
+}
+
+int main(void)
+{
+	t_long 	index;
+	index.path = "test/output-onlinepngtools.xpm";
+	index.path2 = "test/open.xpm";
+
+	index.fd = open("test/test.ber", O_RDONLY);
+	check_size();
+	index.str = get_next_line(index.fd);
+	if (index.str == NULL)
+		return 0;
+	index.mlx = mlx_init();
+	index.win = mlx_new_window(index.mlx, 1200, 610, " new window ");
+	index.img1 = mlx_xpm_file_to_image(index.mlx, index.path, &index.x, &index.y);
+	index.img2 = mlx_xpm_file_to_image(index.mlx, index.path2, &index.x, &index.y);
+	check_event(index.path, index.path2, &index);
+	mlx_key_hook(index.win, key_hook, &index);
+	mlx_loop(index.mlx);
 }
